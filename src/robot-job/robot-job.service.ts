8eea3033dc82fbde6f5038cb55e7bbf4627e2f18
rishav raj <rishav21556@iiitd.ac.in>
2025-06-13 00:00:08 +0530

    Fix path in create task

--- src/robot-job/robot-job.service.ts
+++ src/robot-job/robot-job.service.ts
@@ -551,7 +551,7 @@ export class RobotJobService {
     operationType: string,
     input: any,
   ): Promise<any> {
-    const filePath = `${configFolderName}/${operationType}.json`;
+    const filePath = `src/config_mapping/${configFolderName}/${operationType}.json`;
     try {
       const fileContent = await fs.readFile(filePath, 'utf-8');
       const jsonData = JSON.parse(fileContent);
