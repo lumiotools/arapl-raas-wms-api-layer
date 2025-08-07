46859b6a01b305d5f6803dd3aa92cd62098a1807
DawoodKhatri <dawoodkhatri18@gmail.com>
2025-08-07 20:51:23 +0530

    debug log

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
+  console.log(`Trying to run on port ${process.env.PORT ?? 3000}`);
   const app = await NestFactory.create(AppModule);
     const config = new DocumentBuilder()
     .setTitle('My API')
@@ -33,7 +34,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document); 
-  console.log(`Trying to run on port ${process.env.PORT ?? 3000}`);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
