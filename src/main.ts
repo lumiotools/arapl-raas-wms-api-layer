419b7812e35b1a5f024f5104a55f0706433eb874
DawoodKhatri <dawoodkhatri18@gmail.com>
2025-08-07 20:45:38 +0530

    debug log

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document); 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  console.log(`Trying to run on port ${process.env.PORT ?? 3000}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
