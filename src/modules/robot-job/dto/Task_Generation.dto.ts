bcfc773196b275b44549e467010ec8c9d356729b
rishav raj <rishav21556@iiitd.ac.in>
2025-09-18 12:06:55 +0530

    correct the Location Action Enum Type

--- src/modules/robot-job/dto/Task_Generation.dto.ts
+++ src/modules/robot-job/dto/Task_Generation.dto.ts
@@ -289,8 +289,8 @@ export enum FallbackAction {
 export enum LocationAction {
   PICK = 'PICK',
   DROP = 'DROP',
-  NOP_RESUME = 'NOP-resume',
-  NOP_PAUSE = 'NOP-pause',
+  NOP_RESUME = 'NOP_RESUME',
+  NOP_PAUSE = 'NOP_PAUSE',
 }
 
 export enum LocationType {
