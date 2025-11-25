12cce98d3b2c5e940adb3bfa72399ed5d7dae5cc
DawoodKhatri <dawoodkhatri18@gmail.com>
2025-11-25 11:25:34 +0530

    handled null cargos

--- src/modules/integration/services/create_task.ts
+++ src/modules/integration/services/create_task.ts
@@ -37,9 +37,9 @@ export async function createTask(payload: struct_fms_create_task): Promise<TaskG
                     location_type: task.end_location.location_type,
                     location_action: task.end_location.location_action,
                 },
-                cargos: task.cargos.map(cargo => ({
+                cargos: task.cargos?.map(cargo => ({
                     cargo_code: cargo.cargo_code,
-                })),
+                })) ?? [],
             })),
         }
         // let URL = process.env.FMS_BASE_URL;
